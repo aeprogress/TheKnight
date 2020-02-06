d4b302daa3aab45d59feb19052a03360e6e36e70
aeprogress <55446139+aeprogress@users.noreply.github.com>
2020-02-06 16:15:01 +0300

    Add files via upload

--- Chess/JS/App.js
+++ Chess/JS/App.js
@@ -1,31 +1,38 @@
-$(document).ready(function () {
-
+$(document).ready(function start() {
     let board = new Chess();
     let knight = new Knight(0, 0);
 
     board.possibilities(knight.row, knight.col);
     board.draw(knight.row, knight.col);
-    $('.grid-item').click(function () {
+    $("#start").click(function () {
 
-        $('.possi').click(function () {
+        $('.grid-item').click(function () {
+            $('.possi').click(function update() {
 
-            $("#knight").remove();
-            $(".possi").remove();
+                $("#knight").remove();
+                $(".possi").remove();
 
-            let index = $(this).attr("id");
-            for (let i = 0; i < board.rowPosi.length; i++) {
-                if (i == index) {
-                    knight.row = board.rowPosi[i];
-                    knight.col = board.colPosi[i];
+                let index = $(this).attr("id");
+                for (let i = 0; i < board.rowPosi.length; i++) {
+                    if (i == index) {
+                        knight.row = board.rowPosi[i];
+                        knight.col = board.colPosi[i];
+                    }
                 }
-            }
-            board.possibilities(knight.row, knight.col);
-            board.reset(knight.row, knight.col);
-
-            $("#knight").show();
-            $(".possi").show();
+                board.possibilities(knight.row, knight.col);
+                board.reset(knight.row, knight.col);
 
 
+                $("#knight").show();
+                $(".possi").show();
+                
+            });
+            
         });
+        board.startTimer();
+    });
+    
+    $("#replay").click(function() {
+        document.location.reload();
     });
 });
--- Chess/JS/Chess.js
+++ Chess/JS/Chess.js
@@ -1,10 +1,12 @@
  class Chess {
 
-     constructor(chess, rowPosi, colPosi, numOfPosi) {
+     constructor(numOfPosi) {
          this.chess = new Array(8);
          this.numOfPosi = numOfPosi;
          this.colPosi = new Array(this.numOfPosi);
          this.rowPosi = new Array(this.numOfPosi);
+         this.steps = 1;
+         this.greenSpots = 0;
      }
 
      draw(row, col) {
@@ -26,13 +28,13 @@
                              "<div id='knight'></div></div>";
                      } else {
                          markup = "<div class='grid-item even' id='" +
-                         (offset) + "'></div>";
+                             (offset) + "'></div>";
 
-                         for(let k = 0; k < this.rowPosi.length; k++){
-                             if(i == this.rowPosi[k] && j == this.colPosi[k]){
+                         for (let k = 0; k < this.rowPosi.length; k++) {
+                             if (i == this.rowPosi[k] && j == this.colPosi[k]) {
                                  markup = "<div class='grid-item even' id='" +
-                                 (offset) + "'><div class='possi'"
-                                 + " id ='" + k + "'></div>";
+                                     (offset) + "'><div class='possi'" +
+                                     " id ='" + k + "'></div>";
                              }
                          }
                      }
@@ -43,15 +45,15 @@
                              "<div id='knight'></div></div>";
                      } else {
                          markup = "<div class='grid-item odd' id='" +
-                         (offset) + "'></div>";
+                             (offset) + "'></div>";
 
-                         for(let k = 0; k < this.rowPosi.length; k++){
-                            if(i == this.rowPosi[k] && j == this.colPosi[k]){
-                                markup = "<div class='grid-item odd' id='" +
-                                (offset) + "'><div class='possi'"
-                                + "id = ' "+ k + "'></div>";
-                            }
-                        }
+                         for (let k = 0; k < this.rowPosi.length; k++) {
+                             if (i == this.rowPosi[k] && j == this.colPosi[k]) {
+                                 markup = "<div class='grid-item odd' id='" +
+                                     (offset) + "'><div class='possi'" +
+                                     "id = ' " + k + "'></div>";
+                             }
+                         }
 
                      }
                  }
@@ -66,63 +68,106 @@
 
      possibilities(row, col) {
 
-       var xPosiMoves = [-2, -2, -1, -1, 1, 1, 2, 2];
-       var yPosiMoves = [-1, 1, -2, 2, -2, 2, -1, 1];
-       var posX = 0, posY = 0;
+         var xPosiMoves = [-2, -2, -1, -1, 1, 1, 2, 2];
+         var yPosiMoves = [-1, 1, -2, 2, -2, 2, -1, 1];
+         var posX = 0, posY = 0;
 
-       this.numOfPosi = 0;
-       this.rowPosi = [];
-       this.colPosi = [];
+         this.numOfPosi = 0;
+         this.rowPosi = [];
+         this.colPosi = [];
 
-       for(let i = 0; i < 8; i ++){
-           posX = row + xPosiMoves[i];
-           posY = col + yPosiMoves[i];
+         for (let i = 0; i < 8; i++) {
+             posX = row + xPosiMoves[i];
+             posY = col + yPosiMoves[i];
 
-           if((posX < 0 || posX > 7) || (posY < 0 || posY > 7)){
-               continue;
-           } else {
-               this.rowPosi[this.numOfPosi] = posX;
-               this.colPosi[this.numOfPosi] = posY;
+             if ((posX < 0 || posX > 7) 
+             || (posY < 0 || posY > 7)) {
+                 continue;
+             } else {
+                 this.rowPosi[this.numOfPosi] = posX;
+                 this.colPosi[this.numOfPosi] = posY;
 
-               console.log("\n" + (this.numOfPosi + 1) 
-               + ") Row: " + this.rowPosi[this.numOfPosi] 
-               + " Column: " + this.colPosi[this.numOfPosi] + "\n");
-               this.numOfPosi++;
-           }
-       }
+                 console.log("\n" + (this.numOfPosi + 1) +
+                     ") Row: " + this.rowPosi[this.numOfPosi] +
+                     " Column: " + this.colPosi[this.numOfPosi] + "\n");
+                 this.numOfPosi++;
+             }
+         }
+     }
+
+     startTimer() {
+         let min = 0;
+         let sec = 1;
+         let id = setInterval(() => {   
+             if(min < 10 && sec < 10) {
+                 $("#timer").html("0" + min + " : 0" + sec);
+                } else if(min < 10){
+                    $("#timer").html("0" + min + " : " + sec);
+                } else {
+                    $("#timer").html( min + " : " + sec);
+                }
+
+             if (sec == 60) {
+                 sec = 0;
+                 min++;
+             } else if (this.greenSpots == 64) {
+                clearInterval(id);
+            } else {
+                 sec++;
+             }
+
+         }, 1000);
+
      }
 
      reset(row, col) {
 
-         var offset = 0;
-         var markup = "";
-         var possiMarkup = "";
-
-         for (var i = 0; i < 8; i++) {
-             this.chess[i] = new Array(8)
-         }
+         let offset = 0;
+         let markup = "";
+         let possiMarkup = "";
 
          for (var i = 0; i < 8; i++) {
              for (var j = 0; j < 8; j++) {
 
-                     if (i == row && j == col) {
-                         markup = "<div id='knight'></div></div>";
-                         $("div#" + offset).append(markup);
+                 if (i == row && j == col) {
+                     markup = "<div id='knight'></div>";
+                     /*if($("#" + offset).css("background-color") === "rgb(133, 92, 64)" 
+                     || $("#" + offset).css("background-color") === "rgb(255, 217, 174)") {
+                        $("#" + offset).css("background-color",
+                        "rgba(128, 255, 0)");
+                        this.greenSpots++;
+                        //console.log(this.greenSpots);
+                        }*/ 
+                     $("#" + offset).append(markup);
+                     $("#steps").html( this.steps + " <img src='horse.png'>");
+                     this.steps++;
+                 }
+                 for (let k = 0; k < this.numOfPosi; k++) {
+                     if (i == this.rowPosi[k] && j == this.colPosi[k]) {
+                         possiMarkup = "<div class='possi'" +
+                             " id ='" + k + "'></div>";
+                         $("#" + offset).append(possiMarkup);
+
+                         if($("#" + offset).css("background-color") === "rgb(133, 92, 64)" 
+                         || $("#" + offset).css("background-color") === "rgb(255, 217, 174)") {
+                             
+                            $("#" + offset).css("background-image",
+                            "none");
+                            $("#" + offset).css("background-image",
+                            "url('../guardian.png')");
+                            $("#" + offset).css("background-color",
+                            "rgb(60,179,113)");
+                            this.greenSpots++;
+                            //console.log(this.greenSpots);
+                            } 
                      }
-                     for(let k = 0; k < this.numOfPosi; k++){
-                        if(i == this.rowPosi[k] && j == this.colPosi[k]){
-                            possiMarkup = "<div class='possi'"
-                            + " id ='" + k + "'></div>";
-                            $("div#" + offset).append(possiMarkup);
-                            //$(k).show();
-                        }
-                    }
-                    
+                 }
+
                  offset++;
              }
              offset += 1;
          }
-         
+
      }
 
  }
